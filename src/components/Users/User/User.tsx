f231dccb546cb7cf2e7abf8bf05c88e4e65f079b
Paferov Nikolai <paferov.nikolai@gmail.com>
2023-02-19 22:20:29 +0300

    decompose Users component (change JSX to User component in map method)

---
+++ src/components/Users/User/User.tsx
@@ -0,0 +1,48 @@
+import React, { FC } from "react";
+import styles from "./User.module.css";
+import defaultPhoto from "../../../assets/images/defaultPhoto.jpg";
+import { FollowUnfollow } from "../../common/FollowUnfollow/FollowUnfollow";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../../bll/store";
+import { UserType } from "../../../api/users-api";
+
+type PropsType = {
+  user: UserType;
+};
+
+export const User: FC<PropsType> = ({ user }) => {
+  const navigate = useNavigate();
+
+  const isRequestProcessing = useAppSelector((state) => state.app.isRequestProcessing);
+
+  const handleMoveToUserProfile = (userId: number) => {
+    if (isRequestProcessing) {
+      return;
+    }
+    navigate(`/profile/${userId}`);
+  };
+
+  return (
+    <div className={styles.userArea}>
+      <div className={styles.userData}>
+        <img
+          className={styles.userPhoto}
+          src={
+            user.photos.small
+              ? user.photos.small
+              : user.photos.large
+              ? user.photos.large
+              : defaultPhoto
+          }
+          alt="userPhoto"
+          onClick={() => handleMoveToUserProfile(user.id)}
+        />
+        <div className={styles.userInfo}>
+          <b>{user.name}</b>
+          <span>Status: {user.status ? user.status : "-----"}</span>
+        </div>
+      </div>
+      <FollowUnfollow followed={user.followed} userId={user.id} />
+    </div>
+  );
+};
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,20 +2,16 @@ import React, { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../bll/store";
 import { getUsersTC, setCurrentPageAC, setUsersCountPerPageAC } from "../../bll/users-reducer";
 import styles from "./Users.module.css";
-import defaultPhoto from "../../assets/images/defaultPhoto.jpg";
 import { PaginationBlock } from "../common/PaginationBlock/PaginationBlock";
-import { useNavigate } from "react-router-dom";
-import { FollowUnfollow } from "../common/FollowUnfollow/FollowUnfollow";
+import { User } from "./User/User";
 
 export const Users = () => {
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
   const users = useAppSelector((state) => state.usersPage.users);
   const totalUsersCount = useAppSelector((state) => state.usersPage.totalUsersCount);
   const currentPage = useAppSelector((state) => state.usersPage.currentPage);
   const usersCountPerPage = useAppSelector((state) => state.usersPage.usersCountPerPage);
-  const isRequestProcessing = useAppSelector((state) => state.app.isRequestProcessing);
 
   const onChangeCurrentPage = (page: number) => {
     dispatch(setCurrentPageAC(page));
@@ -29,13 +25,6 @@ export const Users = () => {
     dispatch(getUsersTC(currentPage, usersCountPerPage));
   }, [dispatch, totalUsersCount, currentPage, usersCountPerPage]);
 
-  const handleMoveToUserProfile = (userId: number) => {
-    if (isRequestProcessing) {
-      return;
-    }
-    navigate(`/profile/${userId}`);
-  };
-
   return (
     <div className={styles.content}>
       <PaginationBlock
@@ -48,27 +37,7 @@ export const Users = () => {
         onChangeItemsCountPerPage={onChangeUsersCountPerPage}
       />
       {users.map((user) => (
-        <div key={user.id} className={styles.userArea}>
-          <div className={styles.userData}>
-            <img
-              className={styles.userPhoto}
-              src={
-                user.photos.small
-                  ? user.photos.small
-                  : user.photos.large
-                  ? user.photos.large
-                  : defaultPhoto
-              }
-              alt="userPhoto"
-              onClick={() => handleMoveToUserProfile(user.id)}
-            />
-            <div className={styles.userInfo}>
-              <span>{user.name}</span>
-              <span>{user.status}</span>
-            </div>
-          </div>
-          <FollowUnfollow followed={user.followed} userId={user.id} />
-        </div>
+        <User key={user.id} user={user} />
       ))}
     </div>
   );
