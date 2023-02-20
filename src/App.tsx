0f74a86bed53c33099146c39434c2be8c5723a98
Paferov Nikolai <paferov.nikolai@gmail.com>
2023-02-20 11:00:36 +0300

    remove BrowserRouter from App component and add to index.tsx, change routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Header } from "./components/Header/Header";
 import { Navbar } from "./components/Navbar/Navbar";
 import styles from "./App.module.css";
 import { Dialogs } from "./components/Dialogs/Dialogs";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { News } from "./components/News/News";
 import { Music } from "./components/Music/Music";
 import { Settings } from "./components/Settings/Settings";
@@ -34,35 +34,33 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <div className={styles.layout}>
-        <div className={styles.header}>
-          <Header />
+    <div className={styles.layout}>
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <div className={styles.body}>
+        <div className={styles.navbar}>
+          <Navbar />
         </div>
-        <div className={styles.body}>
-          <div className={styles.navbar}>
-            <Navbar />
-          </div>
-          <div className={styles.content}>
-            <Routes>
-              <Route element={<ProtectedRoutes userIsAuth={true} redirectTo="/login" />}>
-                <Route path="/" element={<Profile />} />
-                <Route path="/social-network" element={<Profile />} />
-                <Route path="/profile/:userId?" element={<Profile />} />
-                <Route path="/dialogs/*" element={<Dialogs />} />
-              </Route>
-              <Route element={<ProtectedRoutes userIsAuth={false} redirectTo="/profile" />}>
-                <Route path="/login" element={<Login />} />
-              </Route>
-              <Route path="/users" element={<Users />} />
-              <Route path="/news" element={<News />} />
-              <Route path="/music" element={<Music />} />
-              <Route path="/settings" element={<Settings />} />
-            </Routes>
-          </div>
+        <div className={styles.content}>
+          <Routes>
+            <Route element={<ProtectedRoutes userIsAuth={true} redirectTo="/login" />}>
+              <Route path="/" element={<Navigate to="/profile" />} />
+              <Route path="/social-network" element={<Navigate to="/profile" />} />
+              <Route path="/profile/:userId?" element={<Profile />} />
+              <Route path="/dialogs/*" element={<Dialogs />} />
+            </Route>
+            <Route element={<ProtectedRoutes userIsAuth={false} redirectTo="/profile" />}>
+              <Route path="/login" element={<Login />} />
+            </Route>
+            <Route path="/users" element={<Users />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </div>
   );
 }
 
